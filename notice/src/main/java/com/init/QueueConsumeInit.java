a2d40baf117e5864f2f2787de16b08e013b97c75
fcy <fcy15658025326@aliyun.com>
2018-10-08 21:51:13 +0800

    11

--- notice/src/main/java/com/init/QueueConsumeInit.java
+++ notice/src/main/java/com/init/QueueConsumeInit.java
@@ -3,6 +3,8 @@ package com.init;
 import com.common.CommonInfo;
 import com.common.QueueConsume;
 import com.common.QueueData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,6 +14,8 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 @Component
 public class QueueConsumeInit implements CommandLineRunner{
+
+    private final static Logger logger = LoggerFactory.getLogger(QueueConsumeInit.class);
 
     @Autowired
     private QueueConsume queueConsume;
@@ -30,6 +34,13 @@ public class QueueConsumeInit implements CommandLineRunner{
 
         queueConsume.start();
         System.out.println("队列消费者线程创建成功");
+
+        logger.debug("logback 访问debug");
+        logger.info("logback 访问info");
+        logger.warn("logback 访问warn");
+        logger.error("logback 访问error");
+
+
 
     }
 }
